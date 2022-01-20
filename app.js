4d2b27a8c49ea0a9ab8b893825af28339e5aff8a
Apple <apple@192.168.1.6>
2022-01-20 19:50:30 +0530

    fixed error in process.env.port

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
 })
 
-app.listen(process.env,PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("server is running on port 3000");
 });
 app.post("/", function(req, res) {
